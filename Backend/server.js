cc6875c4a8d3ef2aeda05e07b3e8133cd09a755e
OmGarg8700 <108590266+OmGarg8700@users.noreply.github.com>
2023-11-22 22:01:50 +0530

    Server is almost done

--- Backend/server.js
+++ Backend/server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 //     })
 //   );
 
-  const db = mysql.createPool({
+const db = mysql.createPool({
     connectionLimit: 500, // Adjust as needed
     host: process.env.DATABASE_HOST,
     user: process.env.DATABASE_USER,
@@ -39,6 +39,10 @@ app.use(express.json());
 const PORT = 8081;
 
 const verify_token = (token, email, callback) => {
+    if(token === undefined){
+        callback(false);
+        return;
+    }
     jwt.verify(token, process.env.JWT, (err, user) => {
         if (err){
             callback(false);
@@ -62,7 +66,6 @@ app.post('/signup', (req,res)=>{
 
     db.query(sql, values, (err, result) => {
         if (err) {
-            console.error(err);
             return res.status(500).json({ error: 'Internal Server Error' });
         }
         return res.status(200).json(result);
@@ -71,7 +74,6 @@ app.post('/signup', (req,res)=>{
 
 app.post('/login', (req,res)=>{
     const {email, password } = req.body;
-    console.log(email, password);
     // const sql = "SELECT * FROM ci_users WHERE `email` = ? AND `password` = ?";
     const sql = "SELECT * FROM ci_users WHERE `email` = ?";
     const values = [email, password];
@@ -81,8 +83,8 @@ app.post('/login', (req,res)=>{
             return res.status(500).json("Error");
         }
         if (result.length > 0) {
-            result = result["email"];
-            user_pass = result["password"];
+            result = result[0];
+            user_pass = result.password;
         
             const isPasswordCorrect = await bcrypt.compare(
               password,
@@ -95,31 +97,116 @@ app.post('/login', (req,res)=>{
         
             // Store user ID in the session
             
-            req.session.userId = result.id;
+            // req.session.userId = result.id;
             
-        
-            // ... (the rest of your code)
-          } else {  
+            return res.status(200).json("Success")
+          } 
+          else {  
             return res.status(401).json("Unauthorized");
           }
     });
 })
 
 app.get('/balance', (req, res) => {
-    const userId = req.session.userId; // Assuming user ID is stored in the session
-    const sql = "SELECT balance FROM ci_users WHERE id = ?";
-    
-    db.query(sql, [userId], (err, result) => {
-        if (err) {
-            return res.status(500).json({ error: 'Internal Server Error' });
+    const {email} = req.body; 
+    const token = req.query.access_token
+
+    verify_token(token, email, (call_result)=>{
+        if(call_result){
+            const sql = "SELECT balance FROM ci_users WHERE email = ?";
+            db.query(sql, [email], (err, result) => {
+                if (err) {
+                    return res.status(500).json({ error: 'Internal Server Error' });
+                }
+                if (result.length === 0) {
+                    return res.status(404).json({ error: 'User not found' });
+                }
+                const balance = result[0].balance;
+                return res.status(200).json({ balance });
+            });
         }
-        if (result.length === 0) {
-            return res.status(404).json({ error: 'User not found' });
+        else{
+            return res.status(401).json("Unauthorized");
         }
-        const balance = result[0].balance;
-        return res.json({ balance });
-    });
+    })
 });
+
+
+app.post("/change-pass", (req, res)=> {
+    const token = req.query.access_token
+    const {email, password} = req.body;
+
+    verify_token(token, email, (call_result) => {
+        if(call_result){
+            const salt = bcrypt.genSaltSync(10)
+            const new_password = bcrypt.hashSync(password, salt)
+
+            const sql = "UPDATE ci_users SET password = ? WHERE email = ?";
+            db.query(sql, [new_password, email], (err, result) => {
+                if (err) {
+                    return res.status(500).json({ error: 'Internal Server Error' });
+                }
+                if (result.length === 0) {
+                    return res.status(404).json({ error: 'User not found' });
+                }
+                return res.status(200).json("Success");
+            });
+        }
+        else{
+            return res.status(401).json("Unauthorized");
+        }
+    })
+
+})
+
+app.post("/add-history", (req, res) => {
+    const {email, service, price, number, status, code_sms} = req.body;
+    const token = req.query.access_token
+
+    verify_token(token, email, (call_result) => {
+        if(call_result){
+            const options = { timeZone: 'Asia/Kolkata', year: 'numeric', month: '2-digit', day: '2-digit' };
+            const currentDate = new Date().toLocaleString('en-IN', options).split('/').reverse().join('-');
+            
+            const sql= "INSERT INTO ci_number_history (`email`,`service`,`price`, `number`, `status`, `code`, `date`) VALUES (?, ?, ?, ?, ?, ?, ?) ";
+
+            db.query(sql, [email, service, price, number, status, code_sms, currentDate], (err, result) => {
+                if (err) {
+                    console.log(err);
+                    return res.status(500).json({ error: 'Internal Server Error' });
+                }
+                else{
+                    return res.status(200).json("Success");
+                }
+            });
+        }
+        else{
+            return res.status(401).json("Unauthorized");
+        }
+    })
+})
+
+app.get("/get-history", (req, res) => {
+    const {email} = req.body;
+    const token = req.query.access_token
+
+    verify_token(token, email, (call_result) => {
+        if(call_result){
+            const sql= "SELECT * FROM ci_number_history WHERE email = ?";
+            db.query(sql, [email], (err, result) => {
+                if (err) {
+                    return res.status(500).json({ error: 'Internal Server Error' });
+                }
+                else{
+                    return res.status(200).json(result);
+                }
+            });
+        }
+        else{
+            return res.status(401).json("Unauthorized");
+        }
+    })
+})
 
 
 app.get('/', (req, res) => {
@@ -128,21 +215,43 @@ app.get('/', (req, res) => {
 
 
 // For testing purposes of verify_token function
-app.get("/test", async(req, res) => {
-    const token = req.query.access_token
-    console.log(token);
-    console.log(req.body);
-    const {email} = req.body;
-    verify_token(token, email, (result) => {
-        if(result){
-            return res.status(200).json("Success")
-        }
-        else{
-            return res.status(401).json("Unauthorized");
-        }
-    })
-})
+// app.get("/test", async(req, res) => {
+//     const token = req.query.access_token
+//     const {email} = req.body;
+
+//     verify_token(token, email, (result) => {
+//         if(result){
+//             return res.status(200).json("Success")
+//         }
+//         else{
+//             return res.status(401).json("Unauthorized");
+//         }
+//     })
+// })
+
+// app.get('/get_user', (req, res) => {
+//     const {email} = req.body;
+//     const sql = "SELECT * from ci_users WHERE email = ?";
+//     db.query(sql, [email], (err, result) => {
+//         if (err) {
+//             return res.status(500).json({ error: 'Internal Server Error' });
+//         }
+//         if (result.length === 0) {
+//             return res.status(404).json({ error: 'User not found' });
+//         }
+//         return res.status(200).json(result);
+//     });
+// })
 
 app.listen(PORT, ()=>{
     console.log(`listening on port ${PORT}`)
 })
+
+// db.query("SELECT * from ci_number_history ;", (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     else{
+//         console.log(result);
+//     }
+// })
